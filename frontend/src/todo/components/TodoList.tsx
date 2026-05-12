b6f6bbd91deba1970e3d9ec0b9cae35fb6929bfd
LNorza <luiscnorza@gmail.com>
2026-05-12 15:45:09 -0700

    feat(todo): implement hover in todo

--- frontend/src/todo/components/TodoList.tsx
+++ frontend/src/todo/components/TodoList.tsx
@@ -23,7 +23,7 @@ export const TodoList = ({ todos, filter, onRemove, onToggle }: Props) => {
         todos.map((todo) => (
           <div
             key={todo.id}
-            className={`flex items-center gap-3 rounded-xl border bg-card px-4 py-3 transition-all hover:shadow-sm ${
+            className={`flex items-center gap-3 rounded-xl border bg-card px-4 py-3 transition-all hover:shadow-sm  hover:bg-card-secondary ${
               todo.completed ? "border-border/50 opacity-80" : "border-border"
             }`}
           >
